758fb82f44faa82a900690db4fb61b310f70c330
zoya-developer <zoya.dev06@gmail.com>
2026-07-06 04:14:53 -0700

    Add files via upload

---
+++ DAY 2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string name;
+	int age,semester;
+	
+	cout<<"Enter your name = ";
+	
+	cin>>name;
+	
+	cout<<"Your name is " <<name <<endl;
+	
+	cout<<"Enter your age =";
+	
+	cin>>age;
+	
+	cout<<"Your age is = "<<age <<endl;
+	
+	cout<<"Enter your current semester = ";
+	
+	cin>>semester;
+	
+	cout<<"Your current semester is " << semester <<endl;
+}
