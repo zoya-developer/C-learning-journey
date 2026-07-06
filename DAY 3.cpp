af463605c08d864172aab4ad0ca7fdf42de55964
zoya-developer <zoya.dev06@gmail.com>
2026-07-06 04:15:59 -0700

    Add files via upload

---
+++ DAY 3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int fnum,snum,res;
+	
+	cout<<"Enter first number = ";
+	cin>>fnum;
+	
+	cout<<"Enter second number = ";
+	cin>>snum;
+	
+	res= fnum + snum;
+	cout<<"Result is"<<res;
+}
