844153556e34ced28b221006252dd8b710fd441d
zoya-developer <zoya.dev06@gmail.com>
2026-07-06 04:16:53 -0700

    Add files via upload

---
+++ DAY 5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int number;
+	cout<<"Enter any number = ";
+	cin>>number;
+	
+	if(number%2==0)
+	 cout<<"Even"<<endl;
+	else
+	 cout<<"Odd"<<endl;
+	
+}
