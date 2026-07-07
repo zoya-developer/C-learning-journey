19a2060f80b7eca04e5483b8eb8f2b3680958307
zoya-developer <zoya.dev06@gmail.com>
2026-07-07 09:04:41 -0700

    Add files via upload

---
+++ Day7.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age ;
+	string gender;
+	 
+	cout<<"Enter your age = " <<endl;
+	cin>>age;
+	
+	
+	if(age>=18 )
+    {
+    	cout<<"You are Eligible";
+	}
+	else
+	{
+		cout<<"You are not Eligible";
+	}
+}
