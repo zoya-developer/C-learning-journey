ac6a2baae98391d8de743acfd8368356c4bb1aed
zoya-developer <zoya.dev06@gmail.com>
2026-07-11 03:53:44 -0700

    Add files via upload

---
+++ Day-10.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int t;
+	cout<<"Enter any number for table = ";
+	cin>>t;
+	for(int n=1; n<=10 ;n++)
+	{
+		 cout<<t <<"x" <<n <<"=" <<t*n <<endl;	
+	}
+}
