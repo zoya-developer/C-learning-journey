f71b9d3e98ba304a3f9a7ac978b36ceb0def26c1
zoya-developer <zoya.dev06@gmail.com>
2026-07-12 01:08:47 -0700

    Add files via upload

---
+++ Day-11.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int guess=123,n;
+	
+	do
+	{
+	  	cout<<"Enter guess no = ";
+	    cin>>n;
+    }
+	while(n!=guess);
+    cout<<"congratulations";
+}
