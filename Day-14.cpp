273271a9993920ac9a24c3078d4875e392213eed
zoya-developer <zoya.dev06@gmail.com>
2026-07-15 11:28:01 -0700

    Add files via upload

---
+++ Day-14.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1,n2;
+	cout<<"Enter number 1 = "<<endl;
+	cin>>n1;
+	cout<<"Enter number 2 = "<<endl;
+	cin>>n2;
+	if(n1>n2)
+	{
+			cout<<"number 1 is greater"<<endl;
+	}
+	else
+	        cout<<"number 2 is greater"<<endl;
+
+}
